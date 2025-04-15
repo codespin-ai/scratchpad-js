a5661e8053f70239d3041a22eea6927b184ff973
Jeswin Kumar <jeswinpk@agilehead.com>
2025-04-15 20:47:38 +0530

    Add linter

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    // Define ignore patterns here
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      '.git/**',
+      '.vscode/**'
+    ]
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  // Adding strict configuration for more strict linting
+  tseslint.configs.strict,
+  // Adding stylistic configuration for consistent code styling
+  tseslint.configs.stylistic,
+);
